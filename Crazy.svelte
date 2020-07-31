4aa47f7620a05989b50c89474b7826e8ed6fb0c1
Alon <od120162@gmail.com>
2020-07-31 19:04:17 +0300

    added Crazy.svelte

---
+++ Crazy.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+
+    export let visible = true;
+
+	function spin(node, { duration }) {
+		return {
+			duration,
+			css: t => {
+				const eased = elasticOut(t);
+
+				return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${~~(t * 360)},
+						${Math.min(100, 1000 - 1000 * t)}%,
+						${Math.min(50, 500 - 500 * t)}%
+					);`
+			}
+		};
+	}
+</script>
+
+<style>
+
+</style>
+
+
+{#if visible} 
+	<div class="centered" in:spin="{{duration: 8000}}" out:fade>
+        <slot></slot>
+	</div>
+{/if} 
